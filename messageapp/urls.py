0e37188b4cf30f659f5881296a99851332e1c107
shiva <s3r3shiva@gmail.com>
2024-12-22 17:12:17 +0530

    connected to websocket

---
+++ messageapp/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import *
+
+urlpatterns = [
+    path('messge/<str:room_name>/<str:username>/',message,name="message" ), 
+    path('',room,name="room" ), 
+]
---
+++ messageapp/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render,redirect
+from .models import *
+
+
+# Create your views here.
+def room(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        room = request.POST['room']
+        try:
+            get_room = Room.objects.get(room_name=room)
+        except Room.DoesNotExist:
+            new_room = Room.objects.create(room_name=room)
+            new_room.save()
+        return redirect("message",room_name=room,username=username)
+            
+    return render(request,"index.html")
+
+def message(request, room_name, username):
+    get_room = Room.objects.get(room_name=room_name)
+    get_messages = Message.objects.filter(room=get_room)
+    context = {
+        "room_name":room_name,
+        "user":username,
+        "messages": get_messages,
+    }
+    return render(request,"message.html",context)

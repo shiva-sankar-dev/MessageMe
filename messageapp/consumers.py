cbd1ffc158d4d9d9c471da37f6f52ee8b63d5336
shiva <s3r3shiva@gmail.com>
2024-12-24 19:25:35 +0530

    chat UI completed

--- messageapp/consumers.py
+++ messageapp/consumers.py
@@ -16,3 +16,33 @@ class ChatConsumer(AsyncWebsocketConsumer):
 
     async def disconnect(self, close_code):
         await self.channel_layer.group_discard(self.room_name, self.channel_name)
+        
+        
+        
+    async def receive(self, text_data):
+        text_data_json = json.loads(text_data)
+        message = text_data_json
+        
+        print(message,"DDDDDDDDDDDDDDDDDDDDDDDDDDD")
+        event = {
+            'type': 'send_message',
+            'message': message,
+        }
+
+        await self.channel_layer.group_send(self.room_name, event)
+
+    async def send_message(self, event):
+        data = event['message']
+        await self.create_message(data=data)
+        response_data = {
+            'sender': data['sender'],
+            'message': data['message']
+        }
+        await self.send(text_data=json.dumps({'message': response_data}))
+
+    @database_sync_to_async
+    def create_message(self, data):
+        get_room_by_name = Room.objects.get(room_name=data['room_name'])
+        if not Message.objects.filter(message=data['message']).exists():
+            new_message = Message(room=get_room_by_name, sender=data['sender'], message=data['message'])
+            new_message.save()
